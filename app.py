ffbcc75ae301bfefc67c3f4271ad0ac9072141fe
matheus1714 <matheussilvamartins1714@gmail.com>
2023-10-23 22:27:08 -0300

    config new route

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from src.routes.home import home
+from src.routes.captcha import captcha
 
 app = Flask(__name__)
 
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
 app.register_blueprint(home)
+app.register_blueprint(captcha)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
